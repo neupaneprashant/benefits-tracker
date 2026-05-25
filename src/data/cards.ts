3a72a5ee3a1515dcaee5e29f8d73c6a2dd8d64fe
Prashant <neupaneprashant@users.noreply.github.com>
2026-05-25 13:45:46 -0500

    feat: add premium American Express cards to database catalog

--- src/data/cards.ts
+++ src/data/cards.ts
@@ -2304,3 +2304,5 @@ export const CARD_BY_ID: Record<string, Card> = Object.fromEntries(
 );
 
 /* TODO: Add more regional bank card models in next phase */
+
+/* Added Amex Gold, Platinum, Everyday lineups */
